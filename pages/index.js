fcc458c2bb8a8dcd62a2a3a1147bd1454d770204
Eirik S. Morland <eirik@morland.no>
2022-03-23 14:06:48 +0100

    Update with some text

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Bæreveiledning Trondheim</title>
+        <title>Silje’s Bæreveiledning</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -26,12 +26,14 @@ export default function Home() {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Bæreveiledning</span>{' '}
-                <span className="block text-indigo-600 xl:inline">Trondheim</span>
+                <span className="block xl:inline">Siljes</span>{' '}
+                <span className="block text-indigo-600 xl:inline">Bæreveiledning</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+              Bæretøy er et lite stykke magi som gjør at du kan trygge og varme, spise og luftes.
+
+<p className='py-4'>Organiser ditt barselliv i dag med Silje’s Bæreveiledning.</p>
+
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
